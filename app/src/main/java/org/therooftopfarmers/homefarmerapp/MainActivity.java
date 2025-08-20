8f0322452ee4834f765d81d39112ad9f8c27377e
vihaanvp <vihaanparlikar27@gmail.com>
2025-08-20 08:29:05 +0530

    Final Code before discontinue

--- app/src/main/java/org/therooftopfarmers/homefarmerapp/MainActivity.java
+++ app/src/main/java/org/therooftopfarmers/homefarmerapp/MainActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
@@ -22,22 +23,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Set a fixed, always-green hamburger icon directly on the toolbar
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+
+        // Always open the drawer when hamburger is pressed
+        toolbar.setNavigationOnClickListener(v -> {
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.openDrawer(GravityCompat.START);
+        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_plants, R.id.nav_camera, R.id.nav_functions, R.id.nav_about, R.id.nav_contact)
-                .setOpenableLayout(drawer)
-                .build();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment == null) {
+            throw new IllegalStateException("NavHostFragment with id R.id.nav_host_fragment not found. Check your activity_main.xml!");
+        }
+        NavController navController = navHostFragment.getNavController();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_plants, R.id.nav_camera, R.id.nav_functions, R.id.nav_about, R.id.nav_contact
+        ).setOpenableLayout(drawer).build();
+
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
